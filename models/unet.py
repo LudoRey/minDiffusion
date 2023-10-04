61940edc9e0dd3893379a594a386c9ff305b3a4c
LudoRey <ludo1093@gmail.com>
2023-10-04 14:34:51 -0400

    new unet (from TeaPearce)

--- models/unet.py
+++ models/unet.py
@@ -1,129 +1,141 @@
-"""
-Simple Unet Structure.
-"""
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-
-class Conv3(nn.Module):
+class ResidualConvBlock(nn.Module):
     def __init__(
         self, in_channels: int, out_channels: int, is_res: bool = False
     ) -> None:
         super().__init__()
-        self.main = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
-            nn.GroupNorm(8, out_channels),
-            nn.ReLU(),
-        )
-        self.conv = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
-            nn.GroupNorm(8, out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
-            nn.GroupNorm(8, out_channels),
-            nn.ReLU(),
-        )
-
+        '''
+        standard ResNet style convolutional block
+        '''
+        self.same_channels = in_channels==out_channels
         self.is_res = is_res
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.main(x)
         if self.is_res:
-            x = x + self.conv(x)
-            return x / 1.414
+            x1 = self.conv1(x)
+            x2 = self.conv2(x1)
+            # this adds on correct residual in case channels have increased
+            if self.same_channels:
+                out = x + x2
+            else:
+                out = x1 + x2 
+            return out / 1.414
         else:
-            return self.conv(x)
+            x1 = self.conv1(x)
+            x2 = self.conv2(x1)
+            return x2
 
 
 class UnetDown(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int) -> None:
+    def __init__(self, in_channels, out_channels):
         super(UnetDown, self).__init__()
-        layers = [Conv3(in_channels, out_channels), nn.MaxPool2d(2)]
+        '''
+        process and downscale the image feature maps
+        '''
+        layers = [ResidualConvBlock(in_channels, out_channels), nn.MaxPool2d(2)]
         self.model = nn.Sequential(*layers)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-
+    def forward(self, x):
         return self.model(x)
 
 
 class UnetUp(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int) -> None:
+    def __init__(self, in_channels, out_channels):
         super(UnetUp, self).__init__()
+        '''
+        process and upscale the image feature maps
+        '''
         layers = [
             nn.ConvTranspose2d(in_channels, out_channels, 2, 2),
-            Conv3(out_channels, out_channels),
-            Conv3(out_channels, out_channels),
+            ResidualConvBlock(out_channels, out_channels),
+            ResidualConvBlock(out_channels, out_channels),
         ]
         self.model = nn.Sequential(*layers)
 
-    def forward(self, x: torch.Tensor, skip: torch.Tensor) -> torch.Tensor:
+    def forward(self, x, skip):
         x = torch.cat((x, skip), 1)
         x = self.model(x)
-
         return x
 
 
-class TimeSiren(nn.Module):
-    def __init__(self, emb_dim: int) -> None:
-        super(TimeSiren, self).__init__()
+class EmbedFC(nn.Module):
+    def __init__(self, input_dim, emb_dim):
+        super(EmbedFC, self).__init__()
+        '''
+        generic one layer FC NN for embedding things  
+        '''
+        self.input_dim = input_dim
+        layers = [
+            nn.Linear(input_dim, emb_dim),
+            nn.GELU(),
+            nn.Linear(emb_dim, emb_dim),
+        ]
+        self.model = nn.Sequential(*layers)
 
-        self.lin1 = nn.Linear(1, emb_dim, bias=False)
-        self.lin2 = nn.Linear(emb_dim, emb_dim)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.view(-1, 1)
-        x = torch.sin(self.lin1(x))
-        x = self.lin2(x)
-        return x
+    def forward(self, x):
+        x = x.view(-1, self.input_dim)
+        return self.model(x)
 
 
-class NaiveUnet(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, n_feat: int = 256) -> None:
-        super(NaiveUnet, self).__init__()
+class Unet(nn.Module):
+    def __init__(self, in_channels, out_channels, n_feat = 256):
+        super().__init__()
+
         self.in_channels = in_channels
         self.out_channels = out_channels
-
         self.n_feat = n_feat
 
-        self.init_conv = Conv3(in_channels, n_feat, is_res=True)
+        self.init_conv = ResidualConvBlock(in_channels, n_feat, is_res=True)
 
         self.down1 = UnetDown(n_feat, n_feat)
         self.down2 = UnetDown(n_feat, 2 * n_feat)
-        self.down3 = UnetDown(2 * n_feat, 2 * n_feat)
 
-        self.to_vec = nn.Sequential(nn.AvgPool2d(4), nn.ReLU())
+        self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
 
-        self.timeembed = TimeSiren(2 * n_feat)
+        self.timeembed1 = EmbedFC(1, 2*n_feat)
+        self.timeembed2 = EmbedFC(1, 1*n_feat)
 
         self.up0 = nn.Sequential(
-            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, 4, 4),
+            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, 7, 7), # otherwise just have 2*n_feat
             nn.GroupNorm(8, 2 * n_feat),
             nn.ReLU(),
         )
 
-        self.up1 = UnetUp(4 * n_feat, 2 * n_feat)
-        self.up2 = UnetUp(4 * n_feat, n_feat)
-        self.up3 = UnetUp(2 * n_feat, n_feat)
-        self.out = nn.Conv2d(2 * n_feat, self.out_channels, 3, 1, 1)
+        self.up1 = UnetUp(4 * n_feat, n_feat)
+        self.up2 = UnetUp(2 * n_feat, n_feat)
+        self.out = nn.Sequential(
+            nn.Conv2d(2 * n_feat, n_feat, 3, 1, 1),
+            nn.GroupNorm(8, n_feat),
+            nn.ReLU(),
+            nn.Conv2d(n_feat, self.out_channels, 3, 1, 1),
+        )
 
-    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+    def forward(self, x, t):
+        # x is (noisy) image, t is timestep, 
 
         x = self.init_conv(x)
-
         down1 = self.down1(x)
         down2 = self.down2(down1)
-        down3 = self.down3(down2)
+        hiddenvec = self.to_vec(down2)
+        
+        # embed time step
+        temb1 = self.timeembed1(t).view(-1, self.n_feat * 2, 1, 1)
+        temb2 = self.timeembed2(t).view(-1, self.n_feat, 1, 1)
 
-        thro = self.to_vec(down3)
-        temb = self.timeembed(t).view(-1, self.n_feat * 2, 1, 1)
-
-        thro = self.up0(thro + temb)
-
-        up1 = self.up1(thro, down3) + temb
-        up2 = self.up2(up1, down2)
-        up3 = self.up3(up2, down1)
-
+        up1 = self.up0(hiddenvec)
+        up2 = self.up1(up1+ temb1, down2)  # add and multiply embeddings
+        up3 = self.up2(up2+ temb2, down1)
         out = self.out(torch.cat((up3, x), 1))
-
         return out
