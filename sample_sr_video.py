1a49800dec393dfb226622fcfdaf23b8e7e4a5dd
LudoRey <ludo1093@gmail.com>
2023-10-04 14:33:30 -0400

    update sample scripts

--- sample_sr_video.py
+++ sample_sr_video.py
@@ -7,20 +7,19 @@ from torch.utils.data import DataLoader
 
 from models.unet import Unet
 from models.ddpm_cond import cDDPM
-from models.ddim_cond import cDDIM
 
 from datasets.axons_dataset import AxonsDataset
 from datasets.dendrites_dataset import DendritesDataset
 from misc.options import parse_options
+
 from misc.utils import apply_colormap
 
-def sample(opt, n_samples=4):
+def sample(opt, n_samples=1):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Create test Dataset
     dataset = globals()[opt.dataset_name](root="./data", crop=opt.crop, phase="valid")
-    #test = dataset[6]
     test = dataset[0]
     test_confocal = test["confocal"].to(device).repeat(n_samples,1,1,1)
     test_STED = test["STED"].to(device).repeat(n_samples,1,1,1)
@@ -36,13 +35,23 @@ def sample(opt, n_samples=4):
     ddpm.eval()
     with torch.no_grad():
         # Sample test
-        samples = ddpm.sample(test_confocal, device)
-    
-    # Make and save grid
-    xset = torch.cat((test_confocal, samples, test_STED), dim=0)
-    yset = apply_colormap(xset, vmin=-0.8, vmax=1)
-    grid = make_grid(yset, nrow=4)
-    save_image(grid, "samples/sample_"+opt.load_checkpoint+"_"+str(opt.load_epoch)+".png")
+        trajectory, estimates = ddpm.sample(test_confocal, device, return_trajectory=True)
+
+    # Concatenate along width axis
+    frames = torch.cat([trajectory, estimates], dim=3)
+    # Only save one of every 10 intermediate results
+    frames = frames[::10]
+    # Convert to color images
+    frames = apply_colormap(frames, vmin=-0.8, vmax=1)
+    # Convert back to uint8
+    frames = (frames*255).type(torch.uint8)
+    # Convert to TxHxWxC format
+    frames = frames.permute((0,2,3,1))[:,:,:,:3]
+    # Save frames as tensor
+    torch.save(frames, "samples/frames_"+opt.load_checkpoint+"_"+str(opt.load_epoch)+".pt")
+    # Save video (requires OpenCV package)
+    #write_video("samples/generation_"+opt.load_checkpoint+"_"+str(opt.load_epoch)+".avi", frames, fps=20)
+
     
 if __name__ == "__main__":
     opt = parse_options()
