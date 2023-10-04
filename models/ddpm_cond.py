bf4e9d9d1a555554a0719f1f9486ad7753bee2f0
LudoRey <ludo1093@gmail.com>
2023-10-04 14:34:34 -0400

    cDDPM now a subclass of DDPM

--- models/ddpm_cond.py
+++ models/ddpm_cond.py
@@ -1,28 +1,21 @@
 from typing import Dict, Tuple
 
-
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from .ddpm import DDPM
 
-class cDDPM(nn.Module):
+
+class cDDPM(DDPM):
     def __init__(
-        self,
-        eps_model: nn.Module,
-        betas: Tuple[float, float],
-        n_T: int,
-        criterion: nn.Module = nn.MSELoss(),
+            self,
+            eps_model: nn.Module,
+            betas: Tuple[float, float],
+            n_T: int,
+            criterion: nn.Module = nn.MSELoss(),
     ) -> None:
-        super(cDDPM, self).__init__()
-        self.eps_model = eps_model
-
-        # register_buffer allows us to freely access these tensors by name. It helps device placement.
-        for k, v in ddpm_schedules(betas[0], betas[1], n_T).items():
-            self.register_buffer(k, v)
-
-        self.n_T = n_T
-        self.criterion = criterion
+        super().__init__(eps_model, betas, n_T, criterion)
 
     def forward(self, x: torch.Tensor, x_cond: torch.Tensor) -> torch.Tensor:
         """
@@ -42,10 +35,14 @@ class cDDPM(nn.Module):
 
         return self.criterion(eps, self.eps_model(torch.cat((x_t, x_cond), 1), _ts / self.n_T))
 
-    def sample(self, x_cond: torch.Tensor, device) -> torch.Tensor:
+    def sample(self, x_cond: torch.Tensor, device, return_trajectory=False) -> torch.Tensor:
         n_sample = x_cond.shape[0]
         size = x_cond.shape[1:]
         x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
+
+        # storing intermediate results in case return_trajectory=True
+        x = [] # vector containing all x_i's
+        x0_estimates = [] # vector containing all intermediate estimates of x0
 
         # This samples accordingly to Algorithm 2. It is exactly the same logic.
         for i in range(self.n_T, 0, -1):
@@ -53,38 +50,16 @@ class cDDPM(nn.Module):
             eps = self.eps_model(
                 torch.cat((x_i, x_cond), 1), torch.tensor(i / self.n_T).to(device).repeat(n_sample, 1)
             )
+            x0_i = (x_i - eps * self.sqrtmab[i])/self.alphabar_t[i]
             x_i = (
                 self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
                 + self.sqrt_beta_t[i] * z
             )
+            
+            x.append(x_i)
+            x0_estimates.append(x0_i)
 
-        return x_i
-
-
-def ddpm_schedules(beta1: float, beta2: float, T: int) -> Dict[str, torch.Tensor]:
-    """
-    Returns pre-computed schedules for DDPM sampling, training process.
-    """
-    assert beta1 < beta2 < 1.0, "beta1 and beta2 must be in (0, 1)"
-
-    beta_t = (beta2 - beta1) * torch.arange(0, T + 1, dtype=torch.float32) / T + beta1
-    sqrt_beta_t = torch.sqrt(beta_t)
-    alpha_t = 1 - beta_t
-    log_alpha_t = torch.log(alpha_t)
-    alphabar_t = torch.cumsum(log_alpha_t, dim=0).exp()
-
-    sqrtab = torch.sqrt(alphabar_t)
-    oneover_sqrta = 1 / torch.sqrt(alpha_t)
-
-    sqrtmab = torch.sqrt(1 - alphabar_t)
-    mab_over_sqrtmab_inv = (1 - alpha_t) / sqrtmab
-
-    return {
-        "alpha_t": alpha_t,  # \alpha_t
-        "oneover_sqrta": oneover_sqrta,  # 1/\sqrt{\alpha_t}
-        "sqrt_beta_t": sqrt_beta_t,  # \sqrt{\beta_t}
-        "alphabar_t": alphabar_t,  # \bar{\alpha_t}
-        "sqrtab": sqrtab,  # \sqrt{\bar{\alpha_t}}
-        "sqrtmab": sqrtmab,  # \sqrt{1-\bar{\alpha_t}}
-        "mab_over_sqrtmab": mab_over_sqrtmab_inv,  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
-    }
+        if return_trajectory:
+            return torch.cat(x), torch.cat(x0_estimates)
+        else:
+            return x_i
