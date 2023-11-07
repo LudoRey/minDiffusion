e9d58b50b5d9bff18c8239235b9e7f596ed1ce56
LudoRey <ludo1093@gmail.com>
2023-11-07 11:27:44 -0500

    small changes

--- sample_video.py
+++ sample_video.py
@@ -18,39 +18,34 @@ def sample(opt, n_samples=1):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Create test Dataset
-    dataset = globals()[opt.dataset_name](root="./data", crop=opt.crop, phase="valid")
-    test = dataset[0]
-    test_confocal = test["confocal"].to(device).repeat(n_samples,1,1,1)
-    test_STED = test["STED"].to(device).repeat(n_samples,1,1,1)
+    dataset = globals()[opt.dataset_name](root="./data", phase="valid")
+    data = dataset[228]
+    confocal = data["confocal"].to(device).repeat(1,1,1,1)
+    STED = data["STED"].to(device).repeat(1,1,1,1)
 
     img_shape = dataset[0]["STED"].shape
     # Load model
-    ddpm = cDDPM(eps_model=Unet(img_shape[0]*2, img_shape[0], n_feat=128), betas=(1e-4, 0.02), n_T=1000)
+    model = cDDPM(eps_model=Unet(img_shape[0]*2, img_shape[0], n_feat=128), betas=(1e-4, 0.02), n_T=1000)
 
     checkpoint_dir = os.path.join("./checkpoints", opt.load_checkpoint)
-    ddpm.load_state_dict(torch.load(os.path.join(checkpoint_dir, "net_"+str(opt.load_epoch)+".pth")))
-    ddpm.to(device)
+    model.load_state_dict(torch.load(os.path.join(checkpoint_dir, "net_"+str(opt.load_epoch)+".pth")))
+    model.to(device)
 
-    ddpm.eval()
+    model.eval()
     with torch.no_grad():
         # Sample test
-        trajectory, estimates = ddpm.sample(test_confocal, device, return_trajectory=True)
+        trajectory, estimates = model.sample(confocal, return_trajectory=True)
 
-    # Concatenate along width axis
-    frames = torch.cat([trajectory, estimates], dim=3)
-    #frames = trajectory
-    # Only save one of every 10 intermediate results
-    frames = frames[::10]
-    # Convert to color images
-    frames = apply_colormap(frames, vmin=-0.8, vmax=7)
-    # Convert back to uint8
-    frames = (frames*255).type(torch.uint8)
-    # Convert to TxHxWxC format
-    frames = frames.permute((0,2,3,1))[:,:,:,:3]
+    frames = trajectory
+    # (optional) Stack estimates
+    frames = torch.stack([trajectory, estimates])
+    # Only save one of every 100 intermediate results
+    #frames = frames[::100]
     # Save frames as tensor
-    torch.save(frames, "samples/frames_"+opt.load_checkpoint+"_"+str(opt.load_epoch)+".pt")
-    # Save video (requires OpenCV package)
-    #write_video("samples/generation_"+opt.load_checkpoint+"_"+str(opt.load_epoch)+".avi", frames, fps=20)
+    save_dir = f"samples/{opt.load_checkpoint}_{opt.load_epoch}"
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    torch.save(frames, f"{save_dir}/frames.pt")
 
     
 if __name__ == "__main__":
