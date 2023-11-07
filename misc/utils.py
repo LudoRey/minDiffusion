aad30eda724797ef1cc9c5df958948802139a7cb
LudoRey <ludo1093@gmail.com>
2023-11-07 11:31:11 -0500

    save to tif + net params

--- misc/utils.py
+++ misc/utils.py
@@ -1,14 +1,22 @@
-import os
-import csv
-import time
+import tifffile as tif
 import torch
-#import cv2
 from matplotlib import cm   
 
 def apply_colormap(x, vmin=0, vmax=1, cmap=cm.get_cmap('hot')):
-    '''Input: a Bx1xHxW tensor. Output: a Bx3xHxW tensor.'''
+    '''Input: a Bx1xHxW tensor. Output: a Bx4xHxW tensor (alpha channel).'''
     x = torch.clamp(x, vmin, vmax)
     x = (x-vmin)/(vmax-vmin)
     y = cmap(x.squeeze(1).cpu().numpy())
     y = torch.Tensor(y).permute(0,3,1,2) # BxHxWxC -> BxCxHxW
     return y
+
+def save_tensor_as_tif(image, filepath):
+    image = torch.clamp(image, min=0, max=1)
+    image = (image*255).cpu().numpy().astype('uint16')
+    tif.imsave(filepath, image)
+
+def get_number_params(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    return num_params
