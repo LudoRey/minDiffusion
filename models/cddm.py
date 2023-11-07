6d1c8fd4ba0ccbfcd3a102f4ddb0de7c0a0af4d9
LudoRey <ludo1093@gmail.com>
2023-11-07 11:29:15 -0500

    added seed for valid, and y prediction support

--- models/cddm.py
+++ models/cddm.py
@@ -1,156 +1,200 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 
 class cDDM(nn.Module):
-    def __init__(self, eps_model: nn.Module, betas: tuple[float, float], n_T: int, criterion: nn.Module = nn.MSELoss()):
+    def __init__(self, denoising_net, denoising_target="eps", beta1=1e-4, betaT=0.02, T=1000, criterion=nn.MSELoss()):
         '''
         General conditional denoising diffusion model. Sampling method needs to be defined in subclasses.
         '''
         super().__init__()
-        self.eps_model = eps_model
+        self.denoising_net = denoising_net # denoising U-net, forward method outputs an estimate of <denoising_target>
+        if denoising_target in ["eps", "y"]:
+            self.denoising_target = denoising_target
+        else:
+            raise ValueError(f"{denoising_target} is not a valid denoising target. Use either 'eps' or 'y'.")
 
         # register_buffer allows us to freely access these tensors by name
-        for k, v in noise_schedules(betas[0], betas[1], n_T).items():
+        for k, v in noise_schedules(beta1, betaT, T).items():
             self.register_buffer(k, v, persistent=False)
 
-        self.n_T = n_T
+        self.T = T
         self.criterion = criterion
 
-    def forward(self, x, x_cond):
+    def forward_prediction(self, y, x, seed=None):
         '''
-        Sample random time step t.
-        Sample x_t through forward process (using reparametrization with variable eps).
-        Estimates eps from (x_t, x_cond, t) using eps_model.
-        Returns the MSE between real and estimated eps.
-        '''
-        # Sample timestep uniformly
-        t = torch.randint(1, self.n_T + 1, (x.shape[0],)).to(x.device)
-        # Sample x_t from forward process through reparametrization
-        eps = torch.randn_like(x)  # eps ~ N(0, 1)
-        x_t = self.sqrtab[t,None,None,None] * x + self.sqrtmab[t,None,None,None] * eps
-        # Estimate eps using eps_model
-        estimated_eps = self.eps_model(torch.cat((x_t, x_cond), 1), t / self.n_T)
+        Used by the forward method, and useful for visualizing the process.
 
+        Sample random time step t.
+        Sample y_t through forward process (using reparametrization with variable eps).
+        Estimates <denoising_target> (either eps or y) from (y_t, x, t) using denoising_net.
+        Returns t, eps, y_t, and estimated_target
+        '''
+        device = y.device
+        generator = torch.Generator(device)
+        if seed is None:
+            seed = generator.seed() # get a random seed : default seed of generator is deterministic
+        generator.manual_seed(seed)
+        # Sample timestep uniformly
+        t = torch.randint(1, self.T + 1, (y.shape[0],), device=device, generator=generator)
+        # Sample y_t from forward process through reparametrization
+        eps = torch.randn(y.shape, device=device, generator=generator)  # eps ~ N(0, 1)
+        y_t = y*self.sqrt_gamma[t,None,None,None] + eps*self.sqrt_one_minus_gamma[t,None,None,None]
+        # Estimate denoising target using denoising_net
+        estimated_target = self.denoising_net(torch.cat((y_t, x), dim=1), t / self.T)
+        return t, eps, y_t, estimated_target
+
+    def forward(self, y, x, seed=None):
+        '''
+        Run forward_prediction method to get t, eps, y_t, and an estimate of the target.
+        Compute estimated eps from estimated target.
+        Returns the criterion loss between real and estimated eps.
+        '''
+        t, eps, y_t, estimated_target = self.forward_prediction(y, x, seed)
+        estimated_eps = self.get_eps_from_target(estimated_target, y_t, t)
         return self.criterion(eps, estimated_eps)
+    
+    def get_eps_from_target(self, target, y_t, t):
+        if self.denoising_target=="eps":
+            eps = target 
+        elif self.denoising_target=="y":
+            eps = (y_t - target*self.sqrt_gamma[t,None,None,None])/self.sqrt_one_minus_gamma[t,None,None,None]
+        return eps
+    
+    def get_y_from_target(self, target, y_t, t):
+        if self.denoising_target=="eps":
+            y = (y_t - target*self.sqrt_one_minus_gamma[t,None,None,None])/self.sqrt_gamma[t,None,None,None]
+        elif self.denoising_target=="y":
+            y = target
+        return y
 
 
 class cDDPM(cDDM):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def sample(self, x_cond, return_trajectory=False, seed=None):
-        '''x_cond is a BxCxHxW tensor. Returns a tensor of the same shape.'''
-        # Get device and B (number of samples) from x_cond
-        device = x_cond.device
-        n_sample = x_cond.shape[0]
+    def sample(self, x, return_trajectory=False, seed=None):
+        '''x is a BxCxHxW tensor. Returns a tensor of the same shape.'''
+        # Get device and B (number of samples) from x
+        device = x.device
+        n_sample = x.shape[0]
         # Set generator and (optional) seed for reproducibility
         generator = torch.Generator(device)
         if seed is None:
             seed = generator.seed() # get a random seed : default seed of generator is deterministic
         generator.manual_seed(seed)
         # Storing intermediate results in case return_trajectory=True
-        x = [] # vector containing all x_i's
-        x_0_estimates = [] # vector containing all intermediate estimates of x0
+        y_noisy = [] # list containing all y_t's
+        y_estimates = [] # list containing all intermediate estimates of y_0
 
         # DDPM sampling
-        x_i = torch.randn(x_cond.shape, generator=generator, device=device) # Initialize x_T with pure noise
-        for i in torch.arange(self.n_T, 0, -1):
+        y_t = torch.randn(x.shape, generator=generator, device=device) # Initialize y_T with pure noise
+        for t in tqdm(torch.arange(self.T, 0, -1), desc="Sampling..."):
             # Compute estimate of eps
-            eps_i = self.eps_model(torch.cat((x_i, x_cond), 1), (i / self.n_T).to(device).repeat(n_sample, 1))
-            # Compute estimate of x_0 (useful only if return_trajectory is True)
-            x_0_i = (x_i - eps_i * self.sqrtmab[i])/self.sqrtab[i]
-            # Compute next step of reverse process
-            z = torch.randn(x_cond.shape, generator=generator, device=device) if i > 1 else 0
-            x_i = self.oneover_sqrta[i] * (x_i - eps_i * self.beta_over_sqrtmab[i]) + self.sqrtbeta[i] * z # sigma is sqrt_beta here
+            tt = t.repeat(n_sample)
+            estimated_target = self.denoising_net(torch.cat((y_t, x), 1), (tt / self.T).to(device))
+            # Compute estimate of y_0 (useful only if return_trajectory is True)
+            estimated_y = self.get_y_from_target(estimated_target, y_t, tt)
+            estimated_eps = self.get_eps_from_target(estimated_target, y_t, tt)
             # Store intermediate results
-            x.append(x_i)
-            x_0_estimates.append(x_0_i)
+            y_noisy.append(y_t)
+            y_estimates.append(estimated_y)
+            # Compute next step of reverse process
+            z = torch.randn(x.shape, generator=generator, device=device) if t > 1 else 0
+            y_t = self.oneover_sqrta[t] * (y_t - estimated_eps * self.beta_over_sqrt_one_minus_gamma[t]) + self.sqrtbeta[t] * z # sigma is sqrt_beta here
+        y_noisy.append(y_t) # y_0 
+        y_estimates.append(y_estimates) # not really useful as it is the same element as the prev one, but to make size consistent with y_noisy
 
         if return_trajectory:
-            return torch.cat(x), torch.cat(x_0_estimates)
+            return torch.cat(y_noisy), torch.cat(y_estimates)
         else:
-            return x_i
+            return y_t
 
+# OUTDATED CODE. DDIM sampling and DDPM sampling for SR3 (gamma instead of t, and inverts y and x for the unet)
 
-class cDDIM(cDDM):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# class cDDIM(cDDM):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
 
-    def sample(self, x_cond, tau: torch.Tensor, return_trajectory=False, seed=None):
-        '''x_cond is a BxCxHxW tensor. Returns a tensor of the same shape.'''
-        # Get device and B (number of samples) from x_cond
-        device = x_cond.device
-        n_sample = x_cond.shape[0]
-        # Set generator and (optional) seed for reproducibility
-        generator = torch.Generator(device)
-        if seed is not None:
-            generator.manual_seed(seed)
-        # Storing intermediate results in case return_trajectory=True
-        x = [] # vector containing all x_i's
-        x_0_estimates = [] # vector containing all intermediate estimates of x0
+#     def sample(self, x_cond, tau: torch.Tensor, return_trajectory=False, seed=None):
+#         '''x_cond is a BxCxHxW tensor. Returns a tensor of the same shape.'''
+#         # Get device and B (number of samples) from x_cond
+#         device = x_cond.device
+#         n_sample = x_cond.shape[0]
+#         # Set generator and (optional) seed for reproducibility
+#         generator = torch.Generator(device)
+#         if seed is not None:
+#             generator.manual_seed(seed)
+#         # Storing intermediate results in case return_trajectory=True
+#         x = [] # vector containing all x_i's
+#         x_0_estimates = [] # vector containing all intermediate estimates of x0
 
-        # DDIM sampling
-        x_ti = torch.randn(x_cond.shape, generator=generator, device=device) # Initialize x_T with pure noise
-        for i in reversed(range(len(tau))):
-            ti = tau[i]
-            # Compute estimate of eps
-            eps_ti = self.eps_model(torch.cat((x_ti, x_cond), 1), (ti / self.n_T).to(device).repeat(n_sample, 1))
-            # Compute estimate of x_0
-            x_0_ti = (x_ti - eps_ti*self.sqrtmab[ti])/self.sqrtab[ti]
-            # Compute next step of reverse process
-            if i > 0:
-                tim = tau[i-1]
-                x_ti = self.sqrtab[tim]*x_0_ti + self.sqrtmab[tim]*eps_ti
-            else:
-                x_ti = x_0_ti
-            # Store intermediate results
-            x.append(x_ti)
-            x_0_estimates.append(x_0_ti)
+#         # DDIM sampling
+#         x_ti = torch.randn(x_cond.shape, generator=generator, device=device) # Initialize x_T with pure noise
+#         for i in tqdm(reversed(range(len(tau))), desc="Sampling..."):
+#             ti = tau[i]
+#             # Compute estimate of eps
+#             eps_ti = self.eps_model(torch.cat((x_ti, x_cond), 1), (ti / self.n_T).to(device).repeat(n_sample, 1))
+#             # Compute estimate of x_0
+#             x_0_ti = (x_ti - eps_ti*self.sqrtmab[ti])/self.sqrtab[ti]
+#             # Store intermediate results
+#             x.append(x_ti)
+#             x_0_estimates.append(x_0_ti)
+#             # Compute next step of reverse process
+#             if i > 0:
+#                 tim = tau[i-1]
+#                 x_ti = self.sqrtab[tim]*x_0_ti + self.sqrtmab[tim]*eps_ti
+#             else:
+#                 x_ti = x_0_ti
+#         x.append(x_ti)
+#         x_0_estimates.append(x_0_ti) # not really useful as x_0_0 = x_0_1, but to make size consistent with x
 
-        if return_trajectory:
-            return torch.cat(x), torch.cat(x_0_estimates)
-        else:
-            return x_ti
+#         if return_trajectory:
+#             return torch.cat(x), torch.cat(x_0_estimates)
+#         else:
+#             return x_ti
         
-class SR3(cDDM):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# class SR3(cDDM):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
 
-    def sample(self, x_cond, return_trajectory=False, seed=None):
-        '''x_cond is a BxCxHxW tensor. Returns a tensor of the same shape.'''
-        # Get device and B (number of samples) from x_cond
-        device = x_cond.device
-        n_sample = x_cond.shape[0]
-        # Set generator and (optional) seed for reproducibility
-        generator = torch.Generator(device)
-        if seed is None:
-            seed = generator.seed() # get a random seed : default seed of generator is deterministic
-        generator.manual_seed(seed)
-        # Storing intermediate results in case return_trajectory=True
-        x = [] # vector containing all x_i's
-        x_0_estimates = [] # vector containing all intermediate estimates of x0
+#     def sample(self, x_cond, return_trajectory=False, seed=None):
+#         '''x_cond is a BxCxHxW tensor. Returns a tensor of the same shape.'''
+#         # Get device and B (number of samples) from x_cond
+#         device = x_cond.device
+#         n_sample = x_cond.shape[0]
+#         # Set generator and (optional) seed for reproducibility
+#         generator = torch.Generator(device)
+#         if seed is None:
+#             seed = generator.seed() # get a random seed : default seed of generator is deterministic
+#         generator.manual_seed(seed)
+#         # Storing intermediate results in case return_trajectory=True
+#         x = [] # vector containing all x_i's
+#         x_0_estimates = [] # vector containing all intermediate estimates of x0
 
-        # DDPM sampling
-        x_i = torch.randn(x_cond.shape, generator=generator, device=device) # Initialize x_T with pure noise
-        for i in torch.arange(self.n_T, 0, -1):
-            # Compute estimate of eps
-            noise_level = self.sqrtab[i].to(device).repeat(n_sample, 1)
-            eps_i = self.eps_model(torch.cat((x_cond, x_i), 1), noise_level)
-            # Compute estimate of x_0 (useful only if return_trajectory is True)
-            x_0_i = (x_i - eps_i * self.sqrtmab[i])/self.sqrtab[i]
-            # Compute next step of reverse process
-            z = torch.randn(x_cond.shape, generator=generator, device=device) if i > 1 else 0
-            x_i = self.oneover_sqrta[i] * (x_i - eps_i * self.beta_over_sqrtmab[i]) + self.sqrtbeta[i] * z # sigma is sqrt_beta here
-            # Store intermediate results
-            x.append(x_i)
-            x_0_estimates.append(x_0_i)
+#         # DDPM sampling
+#         x_t = torch.randn(x_cond.shape, generator=generator, device=device) # Initialize x_T with pure noise
+#         for t in tqdm(torch.arange(self.n_T, 0, -1), desc="Sampling..."):
+#             # Compute estimate of eps
+#             noise_level = self.sqrtab[t].to(device).repeat(n_sample, 1)
+#             eps_t = self.eps_model(torch.cat((x_cond, x_t), 1), noise_level)
+#             # Compute estimate of x_0 (useful only if return_trajectory is True)
+#             x_0_t = (x_t - eps_t * self.sqrtmab[t])/self.sqrtab[t]
+#             # Store intermediate results
+#             x.append(x_t)
+#             x_0_estimates.append(x_0_t)
+#             # Compute next step of reverse process
+#             z = torch.randn(x_cond.shape, generator=generator, device=device) if t > 1 else 0
+#             x_t = self.oneover_sqrta[t] * (x_t - eps_t * self.beta_over_sqrtmab[t]) + self.sqrtbeta[t] * z # sigma is sqrt_beta here
+#         x.append(x_t)
+#         x_0_estimates.append(x_0_t)
 
-        if return_trajectory:
-            return torch.cat(x), torch.cat(x_0_estimates)
-        else:
-            return x_i
+#         if return_trajectory:
+#             return torch.cat(x), torch.cat(x_0_estimates)
+#         else:
+#             return x_t
 
 
 def noise_schedules(beta1, betaT, T):
@@ -164,26 +208,26 @@ def noise_schedules(beta1, betaT, T):
     sqrtbeta = torch.sqrt(beta)
     alpha = 1 - beta
     log_alpha = torch.log(alpha)
-    alphabar = torch.cumsum(log_alpha, dim=0).exp()
+    gamma = torch.cumsum(log_alpha, dim=0).exp()
 
-    sqrtab = torch.sqrt(alphabar)
+    sqrt_gamma = torch.sqrt(gamma)
     oneover_sqrta = 1 / torch.sqrt(alpha)
 
-    sqrtmab = torch.sqrt(1 - alphabar)
-    beta_over_sqrtmab = beta / sqrtmab
+    sqrt_one_minus_gamma = torch.sqrt(1 - gamma)
+    beta_over_sqrt_one_minus_gamma = beta / sqrt_one_minus_gamma
 
     return {
         "alpha": alpha,  # \alpha_t
         "oneover_sqrta": oneover_sqrta,  # 1/\sqrt{\alpha_t}
         "sqrtbeta": sqrtbeta,  # \sqrt{\beta_t}
-        "alphabar": alphabar,  # \bar{\alpha_t}
-        "sqrtab": sqrtab,  # \sqrt{\bar{\alpha_t}}
-        "sqrtmab": sqrtmab,  # \sqrt{1-\bar{\alpha_t}}
-        "beta_over_sqrtmab": beta_over_sqrtmab,  # (\beta_t)/\sqrt{1-\bar{\alpha_t}}
+        "gamma": gamma,  # \bar{\alpha_t}
+        "sqrt_gamma": sqrt_gamma,  # \sqrt{\bar{\alpha_t}}
+        "sqrt_one_minus_gamma": sqrt_one_minus_gamma,  # \sqrt{1-\bar{\alpha_t}}
+        "beta_over_sqrt_one_minus_gamma": beta_over_sqrt_one_minus_gamma,  # (\beta_t)/\sqrt{1-\bar{\alpha_t}}
     }
 
-def get_tau(dim, n_T=1000):
+def get_tau(dim, T=1000):
     ii = torch.arange(1, dim+1)
-    c = n_T/dim
+    c = T/dim
     tau = torch.floor(c*ii).type(torch.int16)
     return tau
--- train.py
+++ train.py
@@ -6,7 +6,6 @@ from torch.utils.data import DataLoader
 from torchvision.utils import save_image, make_grid
 
 from models.unet import Unet
-from models.sr3.unet import UNet
 from models.cddm import cDDPM
 
 import os
@@ -27,21 +26,20 @@ def train(opt):
     train_dataloader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
     # Create validation Dataset and Dataloader
     valid_dataset = globals()[opt.dataset_name](root="./data", phase="valid")
-    valid_dataloader = DataLoader(valid_dataset, batch_size=opt.batch_size)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=opt.batch_size, shuffle=False)
     # Get test image
-    test = valid_dataset[0]
+    test = valid_dataset[228]
     test_confocal = test["confocal"].unsqueeze(0).to(device) # unsqueeze needed for batch dimension
     test_STED = test["STED"].unsqueeze(0).to(device)
 
     img_shape = train_dataset[0]["STED"].shape
 
     # Initialize model
-    #model = cDDPM(eps_model=Unet(img_shape[0]*2, img_shape[0], n_feat=128), betas=(1e-4, 0.02), n_T=1000)
-    model = cDDPM(eps_model=UNet(img_shape[0]*2, img_shape[0]), betas=(1e-4, 0.02), n_T=1000)
+    model = cDDPM(denoising_net=Unet(img_shape[0]*2, img_shape[0], n_feat=128), denoising_target='y')
     # Load model from checkpoint
     if opt.load_checkpoint is not None:
         loaded_checkpoint_dir = os.path.join("./checkpoints", opt.load_checkpoint)
-        model.load_state_dict(torch.load(os.path.join(loaded_checkpoint_dir, "net_"+str(opt.load_epoch)+".pth")))
+        model.denoising_net.load_state_dict(torch.load(os.path.join(loaded_checkpoint_dir, f"denoising_net_{opt.load_epoch}.pth")))
     model.to(device)
 
     optim = torch.optim.Adam(model.parameters(), lr=opt.learning_rate)
@@ -60,7 +58,7 @@ def train(opt):
             STED = batch['STED'].to(device)
             confocal = batch['confocal'].to(device)
             # Compute loss (here it is the output of the model)
-            loss = model(x=STED, x_cond=confocal)
+            loss = model(y=STED, x=confocal)
             train_loss += loss.item()
             # Update parameters
             loss.backward()
@@ -68,12 +66,14 @@ def train(opt):
         train_loss /= len(train_dataloader)
         
         model.eval()
+        seed = 0
         valid_loss = 0
         with torch.no_grad():
             for batch in tqdm(valid_dataloader, desc="Computing validation loss..."):
                 STED = batch['STED'].to(device)
                 confocal = batch['confocal'].to(device)
-                loss = model(x=STED, x_cond=confocal)
+                loss = model(y=STED, x=confocal, seed=seed)
+                seed += 1
                 valid_loss += loss.item()
         valid_loss /= len(valid_dataloader)
 
@@ -98,7 +98,7 @@ def train(opt):
                 grid = make_grid(yset, nrow=3)
             # Save sample grid, model, loss
             save_image(grid, logger.checkpoint_dir + f"/sample_{i}.png")
-            torch.save(model.state_dict(), logger.checkpoint_dir + f"/net_{i}.pth")
+            torch.save(model.denoising_net.state_dict(), logger.checkpoint_dir + f"/denoising_net_{i}.pth")
             logger.save_csv_log()
 
 
