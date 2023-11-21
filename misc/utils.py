7815366035b681565cf80cce38d9b40d18f207d4
LudoRey <ludo1093@gmail.com>
2023-11-21 16:01:43 -0500

    timesteps spaces

--- misc/utils.py
+++ misc/utils.py
@@ -17,7 +17,7 @@ def combine_purple_green(channel1, channel2):
 
 def save_tensor_as_tif(image, filepath):
     image = torch.clamp(image, min=0, max=1)
-    image = (image*255).cpu().numpy().astype('uint16')
+    image = (image*255).cpu().numpy().astype('uint8')
     tif.imsave(filepath, image)
 
 def get_number_params(net):
@@ -25,3 +25,28 @@ def get_number_params(net):
     for param in net.parameters():
         num_params += param.numel()
     return num_params
+
+def get_linspace_timesteps(n, T, start_at_one=False):
+    zero_one = torch.linspace(0,1,n)
+    if start_at_one:
+        t = 1+torch.ceil((T-1)*zero_one).type(torch.int64)
+    else:
+        t = torch.ceil(T*zero_one).type(torch.int64)
+    return t
+
+def get_symmetric_logspace_timesteps(n, T, base=100, start_at_one=False):
+    '''Returns tensor of size n (or n-1 if n is even) containing points in [0,T] (or [1,T] if start_at_one=True).
+    The spacing in [0, T/2] is logarithmic, and ]T/2, T] is the same but flipped.'''
+    logspace = torch.logspace(start=0, end=1, steps=(n+1)//2, base=base)
+    zero_mid = 0.5*normalize_zero_one(logspace)
+    mid_one = (1-zero_mid[:-1]).flip(0)
+    zero_one = torch.cat([zero_mid, mid_one])
+    if start_at_one:
+        t = 1+torch.ceil((T-1)*zero_one).type(torch.int64)
+    else:
+        t = torch.ceil(T*zero_one).type(torch.int64)
+    return t
+
+def normalize_zero_one(x):
+    x = (x-x.min())/(x.max()-x.min())
+    return x
